d8fe4002ddfcd1533c0fabfde36f90e2c6af514a
aswathygokul <ksgokul01@gmail.com>
2024-08-31 12:44:20 +0530

    modified

--- src/main/java/com/luminar/sparepartsonline/controller/ServiceController.java
+++ src/main/java/com/luminar/sparepartsonline/controller/ServiceController.java
@@ -1,25 +1,43 @@
 package com.luminar.sparepartsonline.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/service")
 public class ServiceController {
 
     @GetMapping
-    public String servicePage(){
+    public String servicePage() {
 
         return "service";
     }
+
     @GetMapping("/buy-new-parts")
-    public String buyNewPartsPage() {
+    public String buyNewPartsPage(Model model) {
+        model.addAttribute("productName", "Steering");
+        model.addAttribute("productBrand", "ABC");
+        model.addAttribute("productModel", "2023");
+        model.addAttribute("productYear", "2023");
+        model.addAttribute("productPrice", "4500");
+        model.addAttribute("availability", "In Stock");
+
+
         return "buy-new-parts";
     }
 
     @GetMapping("/buy-old-parts")
-    public String buyOldPartsPage() {
+    public String buyOldPartsPage(Model model) {
+        model.addAttribute("productName", "Steering");
+        model.addAttribute("productBrand", "XYZ");
+        model.addAttribute("productModel", "2019");
+        model.addAttribute("productYear", "2019");
+        model.addAttribute("productPrice", "2500");
+        model.addAttribute("availability", "In Stock");
 
         return "buy-old-parts";
     }
@@ -29,4 +47,17 @@ public class ServiceController {
 
         return "sell-old-parts";
     }
+        @PostMapping("/buy-payment")
+        public String upiPaymentPage(@RequestParam String productName,
+                                     @RequestParam String productPrice,
+                                     @RequestParam String upiId,
+                                     Model model) {
+
+            model.addAttribute("productName", productName);
+            model.addAttribute("productPrice", productPrice);
+            model.addAttribute("upiId", upiId);
+            model.addAttribute("confirmationMessage", "UPI Payment initiated!");
+
+            return "payment-confirmation";
+        }
 }
